1ce0102a41bf9c24198f323e814467789a78eb42
Jaideep <jshah.jshah@gmail.com>
2025-01-09 15:53:56 -0800

    Initial Commit

---
+++ Vera/VeraApp.swift
@@ -0,0 +1,17 @@
+//
+//  VeraApp.swift
+//  Vera
+//
+//  Created by Jaideep Shah on 1/9/25.
+//
+
+import SwiftUI
+
+@main
+struct VeraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VeraTests/VeraTests.swift
@@ -0,0 +1,17 @@
+//
+//  VeraTests.swift
+//  VeraTests
+//
+//  Created by Jaideep Shah on 1/9/25.
+//
+
+import Testing
+@testable import Vera
+
+struct VeraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
